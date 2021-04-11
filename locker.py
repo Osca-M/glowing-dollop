2d0785a38289d6de8c6c9bff983c9507f62fb9ba
Osca-M <oscamwongera@gmail.com>
2021-04-11 22:17:07 +0300

    Create credentials class, add methods to generate random password, find credentials and save credentials

--- locker.py
+++ locker.py
@@ -1,21 +1,61 @@
-class User :
+import string
+from random import choice
+
+
+class User:
     """
     create User class that generates new instances of a user.
 
     """
     user_list = []
 
-    def __init__(self,username,password):
+    def __init__(self, username, password):
         """
         method defining the user properties
         """
 
         self.username = username
-        self.password =password
+        self.password = password
 
     def save_user(self):
-            """
+        """
             A method that saves a new user instance into the user list
             """
+        User.user_list.append(self)
 
-            User.user_list.append(self)
+    def get_user(self):
+        for user in User.user_list:
+            if user.username == self.username and user.password == self.password:
+                return True
+        return False
+
+    def find_credentials(self):
+        credentials = [credential for credential in Credentials.credentials_list if credential.username == self.username and credential.password == self.password]
+        return credentials
+
+
+class Credentials:
+    credentials_list = []
+
+    def __init__(self, credential, username, password=None):
+        self.username = username
+        self.password = password
+        self.credential = credential
+
+    def save_credential(self):
+        """
+        Method that saves a user's credentials to a credential's list
+        """
+        Credentials.credentials_list.append(self)
+
+    @staticmethod
+    def generate_random_password(length=None):
+        """
+        Method that generates a random alphanumeric password
+        """
+        if length is None:
+            length = 10
+
+        my_str = string.ascii_uppercase + string.digits + string.ascii_lowercase
+
+        return ''.join(choice(my_str) for _ in range(length))
